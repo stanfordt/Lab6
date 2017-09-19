5406c89b9774c8df0a544e39012021015e123f73
Trey Stanford <stanford_trey@columbusstate.edu>
2017-09-19 16:17:28 +0000

    Added customer database

--- index.php
+++ index.php
@@ -1,11 +1,49 @@
-<html><body>
 <?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
-
-echo 'Hello world from Cloud9!';
+    require_once('database.php');
+    
+    $query = "SELECT firstName, lastName FROM customers order by lastName;";
+    $customers = $db->query($query);
 
 ?>
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>Shop</title>
+    <link rel="stylesheet" type="text/css" href="main.css" />
+</head>
+
+<!-- the body section -->
+<body>
+    <div id="page">
+
+
+    <div id="main">
+
+        <h1>Customer List</h1>
+
+        <div id="content">
+            <!-- display a table of customerss -->
+            <table>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                </tr>
+                <?php foreach ($customers as $customer) : ?>
+                <tr>
+                    <td><?php echo $customer['firstName']; ?></td>
+                    <td><?php echo $customer['lastName']; ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    </div>
+
+    <div id="footer">
+        <p>&copy; <?php echo date("Y"); ?> Shop, Inc.</p>
+    </div>
+
+    </div><!-- end page -->
 </body>
 </html>
